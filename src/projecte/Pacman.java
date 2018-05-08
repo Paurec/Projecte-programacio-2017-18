cd89e830e1ea941dcb2db92bee4958bf2eac6f76
Paurec <37572607+Paurec@users.noreply.github.com>
2018-05-08 08:19:06 +0200

    Update Pacman.java

--- src/projecte/Pacman.java
+++ src/projecte/Pacman.java
@@ -5,10 +5,42 @@
  */
 package projecte;
 
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+
 /**
  *
  * @author alumne
  */
-public class Pacman {
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+
+public class Pacman extends JFrame {
+
+    public Pacman() {
+        
+        initUI();
+    }
     
+    private void initUI() {
+        
+        add(new Board());
+        
+        setTitle("NewMain");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(380, 420);
+        setLocationRelativeTo(null);
+        setVisible(true);        
+    }
+
+    public static void main(String[] args) {
+
+        EventQueue.invokeLater(() -> {
+            Pacman ex = new Pacman();
+            ex.setVisible(true);
+        });
+    }
 }
